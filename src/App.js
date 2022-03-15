e0705fb144acdec17789e1ce4451458f0547d0ba
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-03-15 23:34:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import classes from "./App.module.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <div className={classes.logo}>
+        <h1>Marco Giuliotti</h1>
+        <p>Front-end Developer</p>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import { useRef } from "react";
+
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <nav className={classes.menu}>
+        <ul className={classes.navigation}>
+          <li className={classes.link}>About</li>
+          <li className={classes.link}>What I do</li>
+          <li className={classes.link}>Projects</li>
+          <li className={classes.link}>Contact</li>
+        </ul>
+        <div className={classes.action}>
+          <button className={classes.cvBtn}>CV</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
